d47c48851c6880ae17cd935c2bf23a004042ab85
Cynthia Kiser <cynthiakiser@gmail.com>
2020-06-07 21:16:58 -0700

    Generated a "data" app to manage our Organization and Link data via the Django admin interface

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "civictechindexadmin.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "civictechindexadmin.data.apps.DataConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
